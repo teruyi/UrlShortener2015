18cccaad28087d3e5ce1229e1490000fbb58b414
Ismael Rodríguez Hernández <ismaro.394@gmail.com>
2015-11-26 00:16:29 +0100

    Added EmbedMongo for tests.

---
+++ bangladesh-green/src/main/java/urlshortener/bangladeshgreen/config/TestMongoConfig.java
@@ -0,0 +1,65 @@
+package urlshortener.bangladeshgreen.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import urlshortener.bangladeshgreen.repository.RepositoryPackage;
+import urlshortener.bangladeshgreen.web.WebPackage;
+
+import java.io.IOException;
+
+@Configuration
+@EnableMongoRepositories(basePackageClasses=RepositoryPackage.class)
+@ComponentScan(basePackageClasses=WebPackage.class)
+/**
+ * Config for EmbedMongo, used for testing.
+ */
+public class TestMongoConfig extends AbstractMongoConfiguration {
+    private static final MongodStarter starter = MongodStarter.getDefaultInstance();
+
+
+    @Autowired(required = false)
+    private MongoClientOptions options;
+
+    @Override
+    protected String getDatabaseName() {
+        return "web";
+    }
+
+    @Bean(destroyMethod = "close")
+    public Mongo mongo() throws IOException {
+        Net net = mongod().getConfig().net();
+        //Create new mongoDB for testing
+        return new MongoClient(net.getServerAddress().getHostName() ,net.getPort() );
+    }
+
+    @Bean(destroyMethod = "stop")
+    public MongodProcess mongod() throws IOException {
+        return mongodExe().start();
+    }
+
+    @Bean(destroyMethod = "stop")
+    public MongodExecutable mongodExe() throws IOException {
+        return starter.prepare(mongodConfig());
+    }
+
+    @Bean
+    public IMongodConfig mongodConfig() throws IOException {
+        return new MongodConfigBuilder().version(Version.Main.PRODUCTION).build();
+    }
+
+}
