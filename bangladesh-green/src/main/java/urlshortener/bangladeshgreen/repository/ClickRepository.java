9573f1fa035d1ed16b374c2fe104d19c5f5957b3
Raul Piraces <raul.piraces@gmail.com>
2015-12-07 15:02:06 +0100

    Testing.

--- bangladesh-green/src/main/java/urlshortener/bangladeshgreen/repository/ClickRepository.java
+++ bangladesh-green/src/main/java/urlshortener/bangladeshgreen/repository/ClickRepository.java
@@ -2,6 +2,7 @@ package urlshortener.bangladeshgreen.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Component;
 import urlshortener.bangladeshgreen.domain.Click;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /*
  * Click repository
  */
+@Component
 public interface ClickRepository extends MongoRepository<Click, String> {
 
 	public List<Click> findByHash(String hash);
